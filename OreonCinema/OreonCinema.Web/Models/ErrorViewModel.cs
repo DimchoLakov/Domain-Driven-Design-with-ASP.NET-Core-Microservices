80049d0fe736dfffb5be34900fe19398f6a6793f
Dimcho Lakov <mdsektor@gmail.com>
2020-09-12 21:00:24 +0100

    Add OreonCinema solution(new project)

---
+++ OreonCinema/OreonCinema.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OreonCinema.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
