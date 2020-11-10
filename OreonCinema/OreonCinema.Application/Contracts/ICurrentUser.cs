aa78f1e7e65e379818b84eaef511bc692621ed34
Dimcho Lakov <mdsektor@gmail.com>
2020-11-10 01:15:52 +0000

    Add Roles to ICurrentUser

--- OreonCinema/OreonCinema.Application/Contracts/ICurrentUser.cs
+++ OreonCinema/OreonCinema.Application/Contracts/ICurrentUser.cs
@@ -1,7 +1,11 @@
 ﻿namespace OreonCinema.Application.Contracts
 {
+    using System.Collections.Generic;
+
     public interface ICurrentUser
     {
         string UserId { get; }
+
+        IEnumerable<string> Roles { get; }
     }
 }
